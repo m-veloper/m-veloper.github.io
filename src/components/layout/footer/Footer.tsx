4218bef36eb8d44f753ed984a4818d55ad685335
m-veloper <m_veloper@naver.com>
2022-09-22 15:56:28 +0900

    html 블로그

--- src/components/layout/footer/Footer.tsx
+++
@@ -1,6 +0,0 @@
-import styles from '../../../../styles/Home.module.css';
-
-const Footer = () => (
-  <footer className={styles.footer}>ISSUE NOW ©2021 Created by M_VELOPER</footer>
-);
-export { Footer };
